5c9b1315e82f301a579b1308b64dc17aefb64c4d
cyberx2010 <cyberxbot3@gmail.com>
2025-05-28 14:30:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'dR4ATa7b#f-fBI5KWwIOHRasMrtRauCOiQlpDRpv872U1b7EMPy0' : process.env.SESSION_ID, 
-PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
+PORT: process.env.PORT === undefined ? '4000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
 };
